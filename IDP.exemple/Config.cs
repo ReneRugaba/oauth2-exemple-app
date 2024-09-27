b19a0e47f0e1a4ba97f8becec79534f181feeab2
René Jean RUGABA <RJR@softfluent.com>
2024-09-27 19:39:53 +0200

    init idp

---
+++ IDP.exemple/Config.cs
@@ -0,0 +1,19 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IDP.exemple
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            [
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            ];
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            [];
+
+        public static IEnumerable<Client> Clients =>
+            [];
+    }
+}
