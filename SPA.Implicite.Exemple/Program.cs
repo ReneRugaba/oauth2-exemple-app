d5c101cc46cf10438d1e98524002322d01709d56
René Jean RUGABA <RJR@softfluent.com>
2024-09-27 20:13:27 +0200

    SPA projet removed  and created

--- SPA.Implicite.Exemple/Program.cs
+++ SPA.Implicite.Exemple/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SPA.exemple;
+using SPA.Implicite.Exemple;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
